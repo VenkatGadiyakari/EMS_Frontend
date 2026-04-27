ae6f50e476689e8f4dfb6a02363a677e08eceed2
pradeepmoganti1307 <mogantipradeep1307@gmail.com>
2026-04-27 11:10:40 +0530

    feat(src): add checkout and order status views

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,11 @@ import BuyerRegistrationView from './views/buyer-registration';
 import OrganiserRegistrationView from './views/organiser-registration';
 import EventListingView from './views/event-listing';
 import EventDetailView from './views/event-detail';
-<<<<<<< HEAD
 import { AdminEventsView } from './views/admin-events';
 import { AdminEventDetailView } from './views/admin-event-detail';
 import { AdminDashboardView } from './views/admin-dashboard';
-=======
 import CheckoutView from './views/checkout';
 import OrderStatusView from './views/order-status';
->>>>>>> e18ee76acd10436085115ce4b579c2ecbee12f06
 import './App.css';
 
 // Create a query client
